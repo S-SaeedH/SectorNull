cf138360810a54c1bfbfef8b5bbdd10928f10156
Saeed <202300363@student.polytechnic.bh>
2026-05-27 06:21:48 +0300

    Fixed lighting/models issue

--- Assets/Scripts/Objectives/UHFPSAutoSaveTrigger.cs
+++ Assets/Scripts/Objectives/UHFPSAutoSaveTrigger.cs
@@ -1,16 +1,78 @@
 using UnityEngine;
+using Newtonsoft.Json.Linq;
+using UHFPS.Runtime;
+using UHFPS.Tools;
 
-public class UHFPSAutoSaveTrigger : MonoBehaviour
+public class UHFPSAutoSaveTrigger : MonoBehaviour, ISaveable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Trigger Settings")]
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private bool triggerOnce = true;
+    [SerializeField] private bool disableColliderAfterUse = true;
+
+    [Header("Debug")]
+    [SerializeField] private bool logSave = false;
+
+    private bool used;
+    private Collider triggerCollider;
+
+    private void Awake()
     {
-        
+        triggerCollider = GetComponent<Collider>();
+
+        if (triggerCollider != null)
+            triggerCollider.isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        ApplyUsedState();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggerOnce && used)
+            return;
+
+        if (!other.CompareTag(playerTag))
+            return;
+
+        used = true;
+        ApplyUsedState();
+
+        TriggerAutoSave();
+    }
+
+    public void TriggerAutoSave()
+    {
+        SaveGameManager.SaveGame(true);
+
+        if (logSave)
+            Debug.Log("Autosave triggered.");
+    }
+
+    private void ApplyUsedState()
+    {
+        if (!disableColliderAfterUse)
+            return;
+
+        if (triggerCollider != null)
+            triggerCollider.enabled = !used;
+    }
+
+    public StorableCollection OnSave()
+    {
+        return new StorableCollection()
+        {
+            { "used", used }
+        };
+    }
+
+    public void OnLoad(JToken data)
+    {
+        if (data["used"] != null)
+            used = (bool)data["used"];
+
+        ApplyUsedState();
     }
 }
